de112c86dd1739dfb65fb5206d933021904c7505
Dmitry Glazyrin <glazyrin.d@dbbest.com>
2017-02-07 18:55:03 +0200

    Global Elsa code refactoring started

--- src/main/java/AI/Elsa_AI.java
+++ src/main/java/AI/Elsa_AI.java
@@ -1,18 +1,14 @@
 package AI;
 
 import Board.GameCell;
-import Units.Enums.UnitTypeNames;
 import Units.Interfaces.MeleeUnit;
 import Units.Unit;
 import javafx.application.Platform;
-import javafx.scene.layout.GridPane;
 
 import java.util.ArrayList;
 import java.util.function.Consumer;
 
 import static AI.Elsa_AI.Strategy.*;
-import static Board.Board.getMainBattlefieldGP;
-import static Board.BoardInitializer.getScoreLimit;
 import static Board.BoardInitializer.getTeam1Score;
 import static Board.BoardInitializer.getTeam2Score;
 import static Board.Utils.BoardUtils.*;
@@ -22,23 +18,28 @@ import static Board.Utils.GameCellUtils.*;
  * Created by Dmitriy on 25.12.2016.
  */
 public class Elsa_AI {
-    private GridPane mainGP = getMainBattlefieldGP();
-    private int totalSPNumber = getStrategicalPoints();
-    private boolean strategicalCellPresence = totalSPNumber > 0;
-    private int scoreLimit = getScoreLimit();
+//    private int scoreLimit = getScoreLimit();                                                 [ get the total score limit ]
+//    private int myActiveUnitNumber = getActiveUnitNumber(2);                                  [ get info about units that still can walk during THIS turn ]
+//    private int enemyCapturedSPNumber = getStrategicalPoints(1);                              [ get number of SP captured by player 1 ]
+//    private int myCapturedSPNumber = getStrategicalPoints(1);                                 [ get number of SP captured by Elsa ]
+//    private int totalUnitNumber = getTotalUnitNumber();                                       [ get total unit number on the board ]
+
+//    private int myRangeUnitAmount = getUnitPopularityType(2, UnitTypeNames.RANGE);            [get the amount of units of the certain type]
+//    private int enemyRangeUnitAmount = getUnitPopularityType(1, UnitTypeNames.RANGE);         [get the amount of units of the certain type]
+//    private int myMeleeUnitAmount =  getUnitPopularityType(2, UnitTypeNames.MELEE);           [get the amount of units of the certain type]
+//    private int enemyMeleeUnitAmount = getUnitPopularityType(1, UnitTypeNames.MELEE);         [get the amount of units of the certain type]
+
+//    int enemyUnitsInMeleeRange = getEnemyUnitsInRangeNumber(gc, 2);                           [ get enemy unit NUMBER around the given cell in the given range ]
+//    int enemyUnitsInShotRange = getEnemyUnitsInRangeNumber(gc, gc.getUnit().getShotRange());  [ get enemy unit NUMBER around the given cell in the given range ]
+//    int enemyUnitsInRange = getEnemyUnitsInRangeNumber(gc, 10);                               [ get enemy unit NUMBER around the given cell in the given range ]
+//    int unActivatedSPInRange = getStrategicalCellsInSRange(gc, 10);                           [ get neutral or enemy strategical cell NUMBER around the given cell in the given range ]
+    //***********************EVERY TURN************************************************
+    private int totalSPNumber = getStrategicalPoints();                                       //[ get total strategical cell NUMBER on the board ]
+    private boolean strategicalCellPresence = totalSPNumber > 0;                              //[ if board have any SP than true ]
 
     private int enemyTotalUnitNumber;
     private int myTotalUnitNumber;
-    private int enemyActiveUnitNumber;
-    private int myActiveUnitNumber;
-    private int enemyCapturedSPNumber;
-    private int myCapturedSPNumber;
 
-    private int totalUnitNumber;
-    private int myRangeUnitAmount;
-    private int enemyRangeUnitAmount;
-    private int myMeleeUnitAmount;
-    private int enemyMeleeUnitAmount;
     private int enemyScore = 0;
     private int myScore = 0;
     private int turnNumber = 0;
@@ -50,68 +51,108 @@ public class Elsa_AI {
     private ArrayList<GameCell> enemyUnitGCList;
     private static Elsa_AI elsa = null;
 
+    //***********************EVERY UNIT************************************************
+    private GameCell nearestEnemyCell;
+    private GameCell nearestStrategicalCell;
+    private GameCell nearestPassableCellToEnemy;
+    private GameCell anyPassableCell;
+    private GameCell furtherShootableCell;
+    private GameCell furtherFromEnemyCell;
+    private GameCell nearestPassableCellToSP;
+    private GameCell bestTarget;
+    private Unit currentUnit;
+    private boolean isFast;
+    private boolean isWeak;
+    private boolean canAttackRange;
+    private boolean canAttackMelee;
+    private boolean canActivateSP;
+    //***********************************************************************
+
     enum Strategy{
         PREFER_CAPTURE, PREFER_RETREAT, PREFER_ATTACK_OR_CAPURE, PREFER_DARE_ATTACK, PREFER_CAUTIOUS_ATTACK, PREFER_NORMAL_ATTACK;
     }
 
     private Elsa_AI() {}
-
     public static Elsa_AI getInstance() {
         if (elsa == null)
             elsa = new Elsa_AI();
         return elsa;
     }
 
-    public Strategy getStrategy(){
+    private void scanBoard(){ //Runs every Turn
         turnNumber++;
-        myUnitGCList = getUnitCellList(2);
-        enemyUnitGCList = getUnitCellList(1);
-        myArmyStrength = getTotalUnitCost(2);
-        enemyArmyStrength = getTotalUnitCost(1);
-
-        scanBoard();
-
-        if(turnNumber < 5){
-            strategy = PREFER_CAPTURE;
-        }else
-        if(enemyTotalUnitNumber > myTotalUnitNumber && myScore >= enemyScore && myTotalUnitNumber < 4
-                && turnNumber > 15 && enemyArmyStrength > myArmyStrength){
-            strategy = PREFER_RETREAT;
-        }else
-        if (myArmyStrength - enemyArmyStrength > 100 && myTotalUnitNumber - enemyTotalUnitNumber > 3){
-            strategy = PREFER_DARE_ATTACK;
-        }else
-        if (myArmyStrength - enemyArmyStrength > 0 && myTotalUnitNumber - enemyTotalUnitNumber > 0){
-            strategy = PREFER_ATTACK_OR_CAPURE;
-        }else
-        if (myArmyStrength <= enemyArmyStrength && myTotalUnitNumber - enemyTotalUnitNumber < 0 && myTotalUnitNumber > 4){
-            strategy = PREFER_CAUTIOUS_ATTACK;
-        }else{
-            strategy = PREFER_NORMAL_ATTACK;
-        }
-        return strategy;
+        myUnitGCList = getUnitCellList(2);                      // [ get Arraylist with all GameCells with Units from team 2 on them ]
+        enemyUnitGCList = getUnitCellList(1);                   // [ get Arraylist with all GameCells with Units from team 1 on them ]
+        myArmyStrength = getTotalUnitCost(2);                   // [ get team's 2 total unit cost ]
+        enemyArmyStrength = getTotalUnitCost(1);                // [ get team's 1 total unit cost ]
+        enemyScore = getTeam1Score();                           // [ get team's 1 score ]
+        myScore = getTeam2Score();                              // [ get team's 2 score ]
+        enemyTotalUnitNumber = getTotalUnitNumber(1);           // [ get total team's 1 unit NUMBER on the board ]
+        myTotalUnitNumber = getTotalUnitNumber(2);              // [ get total team's 2 unit NUMBER on the board ]
     }
 
-    public void scanBoard(){
-        enemyCapturedSPNumber = getStrategicalPoints(1);
-        myCapturedSPNumber = getStrategicalPoints(2);
+    private void performGameCellCalculations(GameCell gc){ //Runs every Unit
+        nearestEnemyCell = getNearestEnemyUnitCell(gc, 100);
+        nearestStrategicalCell = getNearestStrategicalCell(gc, 100);
+        nearestPassableCellToEnemy = getNearestPassableCell(gc, nearestEnemyCell);
+        anyPassableCell = getAnyPassableCell(gc);
+        furtherShootableCell = getFurtherShootableCell(gc, nearestEnemyCell);
+        furtherFromEnemyCell = getFurtherPassableCell(gc, nearestEnemyCell);
+        if(nearestStrategicalCell!=null) {
+            nearestPassableCellToSP = getNearestPassableCell(gc, nearestStrategicalCell);
+        }
+        bestTarget = getBestTarget(gc);
+        currentUnit = gc.getUnit();
+        isFast = (currentUnit.getWalkRange() >= 5);
+        isWeak = (currentUnit.getCost() <= 30);
+        canAttackRange = haveEnemyUnitsInShootingRange(gc);
+        canAttackMelee = haveEnemyUnitsInMeleeRange(gc);
+        canActivateSP = haveSPInActivationRange(gc);
+    }
 
-        enemyTotalUnitNumber = getTotalUnitNumber(1);
-        myTotalUnitNumber = getTotalUnitNumber(2);
+    private void resetGameCellCalculations(){
+        nearestEnemyCell = null;
+        nearestStrategicalCell = null;
+        nearestPassableCellToEnemy = null;
+        anyPassableCell = null;
+        furtherShootableCell = null;
+        furtherFromEnemyCell = null;
+        nearestPassableCellToSP = null;
+        bestTarget = null;
+        currentUnit = null;
+        isFast = false;
+        isWeak = false;
+        canAttackRange = false;
+        canAttackMelee = false;
+        canActivateSP = false;
+    }
 
-        enemyActiveUnitNumber = getActiveUnitNumber(1);
-        myActiveUnitNumber = getActiveUnitNumber(2);
+    private Strategy getStrategy(){
+        scanBoard();
 
-        totalUnitNumber = getTotalUnitNumber();
-
-        enemyScore = getTeam1Score();
-        myScore = getTeam2Score();
-
-        myRangeUnitAmount = getUnitPopularityType(2, UnitTypeNames.RANGE);
-        myMeleeUnitAmount = getUnitPopularityType(2, UnitTypeNames.MELEE);
-
-        enemyRangeUnitAmount = getUnitPopularityType(1, UnitTypeNames.RANGE);
-        enemyMeleeUnitAmount = getUnitPopularityType(1, UnitTypeNames.MELEE);
+        if(strategicalCellPresence){
+            if(turnNumber < 5){
+                strategy = PREFER_CAPTURE;
+            }else
+            if(enemyTotalUnitNumber > myTotalUnitNumber && myScore >= enemyScore && myTotalUnitNumber < 4
+                    && turnNumber > 15 && enemyArmyStrength > myArmyStrength){
+                strategy = PREFER_RETREAT;
+            }else
+            if (myArmyStrength - enemyArmyStrength > 100 && myTotalUnitNumber - enemyTotalUnitNumber > 3){
+                strategy = PREFER_DARE_ATTACK;
+            }else
+            if (myArmyStrength - enemyArmyStrength > 0 && myTotalUnitNumber - enemyTotalUnitNumber > 0){
+                strategy = PREFER_ATTACK_OR_CAPURE;
+            }else
+            if (myArmyStrength <= enemyArmyStrength && myTotalUnitNumber - enemyTotalUnitNumber < 0 && myTotalUnitNumber > 4){
+                strategy = PREFER_CAUTIOUS_ATTACK;
+            }else{
+                strategy = PREFER_NORMAL_ATTACK;
+            }
+        }else{
+            strategy = PREFER_DARE_ATTACK;
+        }
+        return strategy;
     }
 
     public void doAction(){
@@ -119,88 +160,103 @@ public class Elsa_AI {
             Strategy turnStrategy = getStrategy();
             System.out.println("[Elsa STRATEGY]: " + turnStrategy.toString());
             for(GameCell gc : myUnitGCList){
+                performGameCellCalculations(gc);
                 applyTactic(turnStrategy, gc);
+                resetGameCellCalculations();
             }
         });
         thread.setName("Elsa_AI EXECUTION THREAD");
         thread.start();
     }
 
-    private void applyTactic(Strategy strategy, GameCell gc){
-//      int enemyUnitsInMeleeRange = getEnemyUnitsInRangeNumber(gc, 2);
-//      int enemyUnitsInShotRange = getEnemyUnitsInRangeNumber(gc, gc.getUnit().getShotRange());
-//      int enemyUnitsInRange = getEnemyUnitsInRangeNumber(gc, 10);
-//      int unActivatedSPInRange = getStrategicalCellsInSRange(gc, 10);
-        String info = "[Elsa info] ";
-        GameCell nearestEnemyCell = getNearestEnemyUnitCell(gc, 100);
-        if(nearestEnemyCell == null) System.out.println(info + "no enemy in range of 100 found");
-
-        GameCell nearestStrategicalCell = getNearestStrategicalCell(gc, 100);
-        if(nearestStrategicalCell == null) System.out.println(info + "no SP in range of 100 found");
-
-        GameCell nearestPassableCellToEnemy = getNearestPassableCell(gc, nearestEnemyCell);
-        if(nearestPassableCellToEnemy == null) System.out.println(info + "no passable cell near enemy found");
-
-        GameCell anyPassableCell = getAnyPassableCell(gc);
-        if(anyPassableCell == null) System.out.println(info + "no passable cell found");
-
-        GameCell furtherShootableCell = getFurtherShootableCell(gc, nearestEnemyCell);
-        if(furtherShootableCell == null) System.out.println(info + "no further shootable cell found");
-
-        GameCell furtherFromEnemyCell = getFurtherPassableCell(gc, nearestEnemyCell);
-        if(furtherFromEnemyCell == null) System.out.println(info + "no further from enemy cell found");
-
-        GameCell nearestPassableCellToSP = null;
-        if(nearestStrategicalCell!=null) {
-            nearestPassableCellToSP = getNearestPassableCell(gc, nearestStrategicalCell);
-            if (nearestPassableCellToSP == null) System.out.println(info + "no passable cell near SP found");
+    private boolean tryToActivateSP(){
+        if(canActivateSP){
+            strategicalCellClick(nearestStrategicalCell);
+            return true;
         }
-        GameCell bestTarget = getBestTarget(gc);
-        if(bestTarget == null) System.out.println(info + "no best target found");
+        return false;
+    }
 
-        Unit currentUnit = gc.getUnit();
-        boolean isFast = (currentUnit.getWalkRange() >= 5);
-        boolean isWeak = (currentUnit.getCost() <= 30);
-        boolean canAttackRange = haveEnemyUnitsInShootingRange(gc);
-        boolean canAttackMelee = haveEnemyUnitsInMeleeRange(gc);
-        boolean canActivateSP = haveSPInActivationRange(gc);
+    private boolean tryToAttackMelee(){
+        if(canAttackMelee){
+            enemyCellClick(bestTarget);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean tryToAttackRange(){
+        if(canAttackRange){
+            enemyCellClick(bestTarget);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean tryToGoToNearestSP(){
+        if (nearestPassableCellToSP != null) {
+            freeCellClick(nearestPassableCellToSP);
+            return true;
+        }
+        return false;
+    }
+    private boolean tryGoToNearestEnemyForMeleeAttack(){
+        if (nearestPassableCellToEnemy != null) {
+            freeCellClick(nearestPassableCellToEnemy);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean tryGoToNearestEnemyForRangeAttack(){
+        if (furtherShootableCell != null) {
+            freeCellClick(furtherShootableCell);
+            return true;
+        }
+        return false;
+    }
+
+    private void goOnAnyFreeCell(){
+        freeCellClick(anyPassableCell);
+    }
+
+    private void applyTactic(Strategy strategy, GameCell gc){
         if(canAttackMelee || canAttackRange){
             System.out.println(gc.getUnit().getName() + " can attack " + bestTarget.getUnit().getName());
         }
-
         switch (strategy){
             case PREFER_CAPTURE:{
                 if(gc.getUnit() instanceof MeleeUnit){
                     unitCellClick(gc);
-                    if(canActivateSP){
-                        strategicalCellClick(nearestStrategicalCell);
+                    if(tryToActivateSP()){
+                        break;
                     }else
-                    if(canAttackMelee){
-                        enemyCellClick(bestTarget);
+                    if(tryToAttackMelee()){
+                        break;
                     }else
-                    if (nearestPassableCellToSP != null) {
-                        freeCellClick(nearestPassableCellToSP);
+                    if (tryToGoToNearestSP()) {
+                        break;
                     }else
-                    if (nearestPassableCellToEnemy != null) {
-                        freeCellClick(nearestPassableCellToEnemy);
+                    if (tryGoToNearestEnemyForMeleeAttack()){
+                        break;
                     }else{
-                        freeCellClick(anyPassableCell);
+                        goOnAnyFreeCell();
                     }
                 }else{
                     unitCellClick(gc);
-                    if(canActivateSP){
-                        strategicalCellClick(nearestStrategicalCell);
+                    if(tryToActivateSP()){
+                        break;
                     }else
-                    if(canAttackRange){
-                        enemyCellClick(bestTarget);
+                    if(tryToAttackRange()){
+                        break;
                     }else
-                    if (nearestPassableCellToSP != null) {
-                        freeCellClick(nearestPassableCellToSP);
+                    if (tryToGoToNearestSP()) {
+                        break;
                     }else
-                    if (furtherShootableCell != null) {
-                        freeCellClick(furtherShootableCell);
+                    if (tryGoToNearestEnemyForRangeAttack()) {
+                        break;
                     }else{
-                        freeCellClick(anyPassableCell);
+                        goOnAnyFreeCell();
                     }
                 }
                 break;
@@ -386,4 +442,3 @@ public class Elsa_AI {
     }
 }
 
-// определить, что рядом со свободной сп нет моего юнита, который может ее активировать, и если он есть - искать другую.
---
+++ src/main/java/AI/TacticalAnalyzer.java
@@ -0,0 +1,7 @@
+package AI;
+
+/**
+ * Created by Glazyrin.D on 2/7/2017.
+ */
+public class TacticalAnalyzer {
+}
